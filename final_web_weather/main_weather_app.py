2dc869bd28fafa8714408d4fb565048264755199
Pran342 <pranishpun7@gmail.com>
2025-05-02 18:06:30 +1000

    Update main_weather_app.py

--- final_web_weather/main_weather_app.py
+++ final_web_weather/main_weather_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 # Disable PyplotGlobalUseWarning
-st.set_option('deprecation.showPyplotGlobalUse', False)
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
